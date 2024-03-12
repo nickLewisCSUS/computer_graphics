dd36ec338c6a5b7954485a07e3a3bf79bf8ef24e
Nick <nlewis4@csus.edu>
2024-03-11 17:35:09 -0700

    Finished SoLBoard implmentation

--- csc133_project/src/main/java/SlRenderer/slSingleBatchRenderer.java
+++ csc133_project/src/main/java/SlRenderer/slSingleBatchRenderer.java
@@ -20,6 +20,7 @@ public class slSingleBatchRenderer {
     public slSingleBatchRenderer() {
         SPOT.window = new slWindow(SPOT.WIN_WIDTH, SPOT.WIN_HEIGHT, SPOT.WIN_POS_X, SPOT.WIN_POS_Y);
         goLBoard = new slGoLBoard(SPOT.MAX_ROWS, SPOT.MAX_COLS);
+        initOpenGL();
     } // public static void main(String[] args)
 
     public void render() {
@@ -32,9 +33,8 @@ public class slSingleBatchRenderer {
             glfwSetErrorCallback(null).free();
         }
         SPOT.window.destroy();
-    } // void render()
+    }
     void renderLoop() {
-        initOpenGL();
         /* Process window messages in the main thread */
         while (!glfwWindowShouldClose(SPOT.window.getWindowID())) {
             glfwPollEvents();
@@ -52,6 +52,7 @@ public class slSingleBatchRenderer {
         glEnable(GL_CULL_FACE);
         glViewport(SPOT.glViewportX, SPOT.glViewportY, SPOT.WIN_WIDTH, SPOT.WIN_HEIGHT);
         glClearColor(SPOT.CLEAR_COLOR_RED, SPOT.CLEAR_COLOR_GREEN, SPOT.CLEAR_COLOR_BLUE, SPOT.CLEAR_COLOR_ALPHA);
+
         SPOT.shader_program = glCreateProgram();
         int vs = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vs,
@@ -65,23 +66,22 @@ public class slSingleBatchRenderer {
         glShaderSource(fs,
                 "uniform vec3 color;" +
                         "void main(void) {" +
-                        " gl_FragColor = vec4(" + SPOT.FRAG_COLOR_RED + ", " + SPOT.FRAG_COLOR_GREEN + ", " +
-                        SPOT.FRAG_COLOR_BLUE + ", " + SPOT.FRAG_COLOR_ALPHA + ");" +
+                        " gl_FragColor = vec4(color, 1.0);" +
                         "}");
         glCompileShader(fs);
         glAttachShader(SPOT.shader_program, fs);
         glLinkProgram(SPOT.shader_program);
         glUseProgram(SPOT.shader_program);
+
         SPOT.vpMatLocation = glGetUniformLocation(SPOT.shader_program, "viewProjMatrix");
-        return;
-    } // void initOpenGL()
+        SPOT.renderColorLocation = glGetUniformLocation(SPOT.shader_program, "color");
+    }
     void renderObjects() {
-            glfwPollEvents();
+            //glfwPollEvents();
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
-            for (int row = 0; row < SPOT.MAX_ROWS; row++) { // Change 2 to the desired number of rows
-                for (int col = 0; col < SPOT.MAX_COLS; col++) { // Change 5 to the desired number of columns
+            for (int row = 0; row < SPOT.MAX_ROWS; row++) {
+                for (int col = 0; col < SPOT.MAX_COLS; col++) {
                     int vbo = glGenBuffers();
                     int ibo = glGenBuffers();
                     int flipRow = 1-row;
@@ -101,11 +101,12 @@ public class slSingleBatchRenderer {
 
                     glBindBuffer(GL_ARRAY_BUFFER, vbo);
                     glBufferData(GL_ARRAY_BUFFER, (FloatBuffer) BufferUtils.createFloatBuffer(vertices.length).put(vertices).flip(), GL_STATIC_DRAW);
+
                     glEnableClientState(GL_VERTEX_ARRAY);
-                    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-                    glBufferData(GL_ELEMENT_ARRAY_BUFFER, (IntBuffer) BufferUtils.createIntBuffer(indices.length).put(indices).flip(), GL_STATIC_DRAW);
                     glVertexPointer(SPOT.glVertexPointerSize, GL_FLOAT, SPOT.glVertexPointerStride, SPOT.glVertexPointerPoint);
 
+                    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+                    glBufferData(GL_ELEMENT_ARRAY_BUFFER, (IntBuffer) BufferUtils.createIntBuffer(indices.length).put(indices).flip(), GL_STATIC_DRAW);
 
 
                     // Update color based on cell state
@@ -131,6 +132,6 @@ public class slSingleBatchRenderer {
             }
 
             glfwSwapBuffers(SPOT.window.getWindowID());
-    } // renderObjects
+    }
 
 }
